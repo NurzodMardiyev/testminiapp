01ada8e6a608f67167e492819db694eb558a6740
NurzodMardiyev <nurzodbekmardiyev1306@gmail.com>
2025-01-17 11:53:27 +0500

    tg

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { useEffect, useState } from "react";
 
 function App() {
+  console.log(window);
   return (
     <div>
       <h1>Telegram Mini App</h1>
+      <p>{window.Telegram}</p>
     </div>
   );
 }
