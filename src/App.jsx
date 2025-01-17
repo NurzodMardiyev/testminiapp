a72627ecbf64be5b26b4f4c8925e2c251b4ec290
NurzodMardiyev <nurzodbekmardiyev1306@gmail.com>
2025-01-17 13:05:18 +0500

    user

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,12 @@ function App() {
 
   return (
     <div>
+      <div>{user}</div>
       {user ? (
         <div>
           <h1>Welcome, {user.first_name}!</h1>
           <p>User ID: {user.id}</p>
+          <p>{user}</p>
         </div>
       ) : (
         <p>Loading user data...</p>
