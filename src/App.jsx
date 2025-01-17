83e6d04de92476d1f62612d2d8a99ed14496afe8
NurzodMardiyev <nurzodbekmardiyev1306@gmail.com>
2025-01-17 11:42:42 +0500

    asa

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,11 @@
 import { useEffect, useState } from "react";
 
-function TelegramApp() {
-  const [userData, setUserData] = useState(null);
-
-  useEffect(() => {
-    // Telegram WebApp API-ni chaqirish
-    const tg = window.Telegram.WebApp;
-
-    // Foydalanuvchi ma'lumotlarini olish
-    const initDataUnsafe = tg.initDataUnsafe;
-    const user = initDataUnsafe?.user;
-
-    if (user) {
-      setUserData(user);
-    }
-
-    // WebApp-ni o'rnatish
-    tg.ready();
-  }, []);
-
+function App() {
   return (
     <div>
       <h1>Telegram Mini App</h1>
-      {userData ? (
-        <div>
-          <p>Foydalanuvchi ismi: {userData.first_name}</p>
-          <p>Familiyasi: {userData.last_name}</p>
-          <p>Username: @{userData.username}</p>
-          <p>ID: {userData.id}</p>
-        </div>
-      ) : (
-        <p>Foydalanuvchi malumotlarini yuklash...</p>
-      )}
     </div>
   );
 }
 
-export default TelegramApp;
+export default App;
